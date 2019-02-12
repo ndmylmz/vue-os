7dd2ad649896b9795e0814af49c94ae21f39c03f
UpolRyskulova <upol@useinsider.com>
2019-02-12 19:48:20 +0300

    test

---
+++ src/components/Statistics.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="statistic">
+      sdfdf
+    <!-- <div class="row">Time: {{teamMetrics}} -> {{teamMetrics}} issues</div>
+    <div class="row">Time: {{teamMetrics}} -> {{teamMetrics}} issues</div>
+    <div class="row">Time: {{teamMetrics}} -> {{teamMetrics}} issues</div> -->
+  </div>
+</template>
+
+<script>
+
+export default {
+   props: ['teamMetrics']
+};
+
+</script>
