245bc021d26a90e0ed751b1fee931b2c5fe20fc8
UpolRyskulova <upol@useinsider.com>
2019-02-17 20:28:51 +0300

    converted mock metrics to display in the table format, and displayed it using Statistics metric

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,50 +1,25 @@
 <template>
-  <div class="col">
-    <div class="statistic">
-      <div v-for="(metrics, stateName) in teamMetrics.data.types" v-bind:key="metrics">
-        <div class="col" v-if="isPreviousWeek == true" style="color: red;">{{stateName}}</div>
-        <div>
-          Time Spend:
-          <span style="color: black;">{{metrics.duration}}</span> for
-          <span style="color: black;">{{metrics.tasksCount}}</span> issues
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- <table class="table table-striped">
-      <thead>
+  <table class="table table-bordered table-info">
+      <thead class="thead-light">
         <tr>
-          <th scope="col">States</th>
-          <th scope="col">Previous 2 Week Metrics</th>
-          <th scope="col">Current 2 Week Metrics</th>
+          <th scope="col">State</th>
+          <th scope="col">Last 2 week Metrics</th>
+          <th scope="col">Current 2 eeks Metrics</th>
           <th scope="col">Comparison</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(column, index) in teamMetrics" v-bind:key="column">
-          <th scope="row">{{column.data.types}}</th>
-          <td>{{column.data.types}}</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
+        <tr v-for="(metrics,index) in teamMetrics" v-bind:key="metrics">
+          <th scope="row">{{states[index]}}</th>
+          <td>{{metrics[states[index]].last.metric.duration}}</td>
+          <td>{{metrics[states[index]].current.metric.duration}}</td>
         </tr>
       </tbody>
-    </table> -->
+    </table>
 </template>
 
 <script>
 export default {
-  props: ["teamMetrics", "isPreviousWeek"]
+  props: ["teamMetrics", "states"]
 };
 </script>
--- src/components/TeamMetrics.vue
+++ src/components/TeamMetrics.vue
@@ -1,15 +1,162 @@
 <template>
   <div class="row metrics">
-    <!-- <statistics :teamMetrics="teamMetrics" :isPreviousWeek="true"></statistics> -->
-    <statistics :teamMetrics="teamMetrics" :isPreviousWeek="true"></statistics>
-    <statistics :teamMetrics="teamMetrics" :isPreviousWeek="false"></statistics>
-
-    
+    <statistics :teamMetrics="teamMetrics" :states="states"></statistics>
+    <!-- <statistics :teamMetrics="teamMetrics" :isPreviousWeek="false"></statistics> -->
   </div>
 </template>
 
 <script>
 import Statistics from "./Statistics.vue";
+
+var metricsValues = [
+  {
+    response: {
+      status: 500
+    },
+    data: {
+      overall: {
+        average: 56,
+        taskCount: 58,
+        crCount: 59,
+        qaCount: 60
+      },
+      types: {
+        inProgress: {
+          duration: "1 day(s), 12 hours, 2 minutes, 23 seconds",
+          tasksCount: 56,
+          tasksLists: [
+            {
+              tasksCode: "OPT-10029",
+              assignee: "APAC OPAM",
+              developer: "John Dev",
+              qa: "Koroglu QA",
+              opam: "Nasreddin OPAM",
+              timeSpent: "23 hours, 45 minutes, 15 seconds"
+            },
+            {
+              tasksCode: "OPT-10002",
+              assignee: "APAC OPAM",
+              developer: "Lorem Dev",
+              qa: "Karagoz QA",
+              opam: "Emre Spartan OPAM",
+              timeSpent: "23 hours, 25 minutes, 49 seconds"
+            }
+          ]
+        },
+        devDone: {
+          duration: "1 day(s), 12 hours, 56 minutes, 31 seconds",
+          tasksCount: 59,
+          tasksLists: [
+            {
+              tasksCode: "OPT-10072",
+              assignee: "TURKEY OPAM",
+              developer: "Cato Dev",
+              qa: "Fatih Terim QA",
+              opam: "Enver Pasha OPAM",
+              timeSpent: "0 hours, 45 minutes, 11 seconds"
+            },
+            {
+              tasksCode: "OPT-10069",
+              assignee: "APAC OPAM",
+              developer: "Lorem Dev",
+              qa: "Fatih Terim QA",
+              opam: "Enver Pasha OPAM",
+              timeSpent: "17 hours, 33 minutes, 0 seconds"
+            }
+          ]
+        }
+      }
+    }
+  },
+  {
+    response: {
+      status: 500
+    },
+    data: {
+      overall: {
+        average: 22,
+        taskCount: 22,
+        crCount: 22,
+        qaCount: 22
+      },
+      types: {
+        inProgress: {
+          duration: "2 day(s), 22 hours, 22 minutes, 22 seconds",
+          tasksCount: 22,
+          tasksLists: [
+            {
+              tasksCode: "OPT-10029",
+              assignee: "APAC OPAM",
+              developer: "John Dev",
+              qa: "Koroglu QA",
+              opam: "Nasreddin OPAM",
+              timeSpent: "23 hours, 45 minutes, 15 seconds"
+            },
+            {
+              tasksCode: "OPT-10002",
+              assignee: "APAC OPAM",
+              developer: "Lorem Dev",
+              qa: "Karagoz QA",
+              opam: "Emre Spartan OPAM",
+              timeSpent: "23 hours, 25 minutes, 49 seconds"
+            }
+          ]
+        },
+        devDone: {
+          duration: "1 day(s), 12 hours, 56 minutes, 31 seconds",
+          tasksCount: 59,
+          tasksLists: [
+            {
+              tasksCode: "OPT-10072",
+              assignee: "TURKEY OPAM",
+              developer: "Cato Dev",
+              qa: "Fatih Terim QA",
+              opam: "Enver Pasha OPAM",
+              timeSpent: "0 hours, 45 minutes, 11 seconds"
+            },
+            {
+              tasksCode: "OPT-10069",
+              assignee: "APAC OPAM",
+              developer: "Lorem Dev",
+              qa: "Fatih Terim QA",
+              opam: "Enver Pasha OPAM",
+              timeSpent: "17 hours, 33 minutes, 0 seconds"
+            }
+          ]
+        }
+      }
+    }
+  }
+];
+
+function convertMetricDataToDisplay(metricsValues) {
+  var states = Object.keys(metricsValues[0].data.types);
+  var convertedMetrics = [];
+
+  for (var index = 0; index < states.length; index++) {
+    var tmp = {};
+
+    tmp[states[index]] = {
+      last: {
+        overall: metricsValues[0].data.overall,
+        metric: metricsValues[0].data.types[states[index]]
+      },
+      current: {
+        overall: metricsValues[1].data.overall,
+        metric: metricsValues[1].data.types[states[index]]
+      }
+    };
+
+    convertedMetrics.push(tmp);
+  }
+
+  return {
+    metrics: convertedMetrics,
+    states: states
+  }
+}
+
+var data = convertMetricDataToDisplay(metricsValues);
 
 export default {
   props: ["teamName"],
@@ -18,67 +165,8 @@ export default {
   },
   data() {
     return {
-      teamMetrics: 
-        {
-          response: {
-            status: 500
-          },
-          data: {
-            overall: {
-              average: 56,
-              taskCount: 58,
-              crCount: 59,
-              qaCount: 60
-            },
-            types: {
-              inProgress: {
-                duration: "1 day(s), 12 hours, 2 minutes, 23 seconds",
-                tasksCount: 56,
-                tasksLists: [
-                  {
-                    tasksCode: "OPT-10029",
-                    assignee: "APAC OPAM",
-                    developer: "John Dev",
-                    qa: "Koroglu QA",
-                    opam: "Nasreddin OPAM",
-                    timeSpent: "23 hours, 45 minutes, 15 seconds"
-                  },
-                  {
-                    tasksCode: "OPT-10002",
-                    assignee: "APAC OPAM",
-                    developer: "Lorem Dev",
-                    qa: "Karagoz QA",
-                    opam: "Emre Spartan OPAM",
-                    timeSpent: "23 hours, 25 minutes, 49 seconds"
-                  }
-                ]
-              },
-              devDone: {
-                duration: "1 day(s), 12 hours, 56 minutes, 31 seconds",
-                tasksCount: 59,
-                tasksLists: [
-                  {
-                    tasksCode: "OPT-10072",
-                    assignee: "TURKEY OPAM",
-                    developer: "Cato Dev",
-                    qa: "Fatih Terim QA",
-                    opam: "Enver Pasha OPAM",
-                    timeSpent: "0 hours, 45 minutes, 11 seconds"
-                  },
-                  {
-                    tasksCode: "OPT-10069",
-                    assignee: "APAC OPAM",
-                    developer: "Lorem Dev",
-                    qa: "Fatih Terim QA",
-                    opam: "Enver Pasha OPAM",
-                    timeSpent: "17 hours, 33 minutes, 0 seconds"
-                  }
-                ]
-              }
-            }
-          }
-        }
-  
+      teamMetrics: data.metrics,
+      states: data.states
     };
   }
 };
